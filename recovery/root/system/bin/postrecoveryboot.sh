1aad2421f7fcc72aaaa852c72209fd8fb91b050d
Shibu Shaji <shibushaji199818@proton.me>
2025-10-28 23:28:47 +0000

    Onyx: Touchscreen fix. `attempt 1`. Imported/load all possible modules/firmwares/libs related to touch from https://dumps.tadiphone.dev/dumps/poco/onyx.git Rewrite/change `init.recovery.qcom.rc` for touch. Made changes to `BoardConfig.mk` to make the touchscreen to work. So far decryption is not implemented.

---
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+#
+# TODO: this kludge is needed to prevent issues with mounting
+# system and vendor in some zip installers and in the gui
+#
+insmod /vendor/lib/modules/1.1/xiaomi_touch.ko
+insmod /vendor/lib/modules/1.1/nt38771_touch.ko
+exit 0
+#
