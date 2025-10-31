1227c47ed310a17b62c1497d23ed6644f9dbbe88
Ravinderrohilla <88099034+Ravinderrohilla@users.noreply.github.com>
2025-10-31 19:55:18 +0530

    Test2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-#TW_SCREEN_BLANK_ON_BOOT := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
